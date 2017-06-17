2efe30bebe1da72d9e0032fff0a34e0d6ee5a02d
hamadu <dercll1020@gmail.com>
2017-06-17 13:14:25 +0900

    command line options

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Blog } from './model/blog'
 import * as IO from './util/io'
 const cmd = require('commander');
@@ -5,21 +7,26 @@ const cmd = require('commander');
 cmd.version('0.0.1')
    .option('-c, --config [path]', 'path to the config.json')
    .option('-o, --out [path]',    'output path')
+   .option('-p, --preview',       'preview after generate', false)
    .parse(process.argv)
 
+if (!cmd.config || !cmd.out) {
+  cmd.help()
+}
 
 const config = IO.readBlogConfig(cmd.config)
 const blog = Blog.generate(config)
 blog.dump(cmd.out)
 
-const stat = require('node-static');
-const file = new stat.Server(cmd.out);
+if (cmd.preview) {
+  const stat = require('node-static');
+  const file = new stat.Server(cmd.out);
+  require('http').createServer((request: any, response: any) => {
+    request.addListener('end', function () {
+      file.serve(request, response)
+    }).resume()
+  }).listen(8080)
 
-require('http').createServer((request: any, response: any) => {
-  request.addListener('end', function () {
-    file.serve(request, response)
-  }).resume()
-}).listen(8080)
-
-const opn = require('opn')
-opn('http://localhost:8080')
+  const opn = require('opn')
+  opn('http://localhost:8080')
+}
