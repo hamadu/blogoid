3775112c78be46155d966cafc3cd6baac7c65bb4
hamadu <dercll1020@gmail.com>
2017-06-17 11:27:01 +0900

    open blog top

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,6 +1,7 @@
 import { Blog } from './model/blog'
 import * as IO from './util/io'
 
+
 const config = IO.readBlogConfig('./sample/config.json')
 const blog = Blog.generate(config)
 blog.dump('./sample/out')
@@ -10,6 +11,9 @@ const file = new stat.Server('./sample/out');
 
 require('http').createServer((request: any, response: any) => {
   request.addListener('end', function () {
-    file.serve(request, response);
-  }).resume();
-}).listen(8080);
+    file.serve(request, response)
+  }).resume()
+}).listen(8080)
+
+const opn = require('opn')
+opn('http://localhost:8080')
